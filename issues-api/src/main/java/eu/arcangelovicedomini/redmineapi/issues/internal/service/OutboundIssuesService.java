b5ddd8eabaff773953ae788dc247587ec081f803
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2017-05-23 22:31:48 +0200

    dev and site

--- issues-api/src/main/java/eu/arcangelovicedomini/redmineapi/issues/internal/service/OutboundIssuesService.java
+++ issues-api/src/main/java/eu/arcangelovicedomini/redmineapi/issues/internal/service/OutboundIssuesService.java
@@ -10,14 +10,32 @@ import eu.arcangelovicedomini.redmineapi.issues.external.service.dto.GetIssuesRe
 @Path("")
 public interface OutboundIssuesService {
 
+	/**
+	 * 
+	 * @param offset
+	 * @param limit
+	 * @param sort
+	 * @param issueId
+	 * @param projectId
+	 * @param subprojectId
+	 * @param trackerId
+	 * @param statusId
+	 * @param assignedToId
+	 * @param parentId
+	 * @return
+	 * @throws RedmineException
+	 */
 	@GET
 	@Path("issues.json")
 	public GetIssuesResponse getIssues(
 			@QueryParam("offset") Integer offset,
 			@QueryParam("limit") Integer limit,
 			@QueryParam("sort") String sort,
-			@QueryParam("issue_id") Long issueId,
-			@QueryParam("project_id") Long projectId,
-			@QueryParam("subproject_id") Long subprojectId,
-			@QueryParam("tracker_id") Long trackerId) throws RedmineException;
+			@QueryParam("issue_id") String issueId,
+			@QueryParam("project_id") String projectId,
+			@QueryParam("subproject_id") String subprojectId,
+			@QueryParam("tracker_id") String trackerId,
+			@QueryParam("status_id") String statusId,
+			@QueryParam("assigned_to_id") String assignedToId,
+			@QueryParam("parent_id") String parentId) throws RedmineException;
 }
