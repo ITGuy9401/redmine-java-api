ca97ad0483e62ebc691c10a87b0c60efb7ac8ee6
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2017-05-24 22:22:37 +0200

    development

--- common/src/main/java/eu/arcangelovicedomini/redmineapi/common/util/CXFServiceUtils.java
+++ common/src/main/java/eu/arcangelovicedomini/redmineapi/common/util/CXFServiceUtils.java
@@ -1,8 +1,31 @@
 package eu.arcangelovicedomini.redmineapi.common.util;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.cxf.jaxrs.client.JAXRSClientFactoryBean;
+
 public class CXFServiceUtils {
 
-	public static <E> E getOutboundService(Class<E> clazz) {
-		return null;
+	public static <E> E getOutboundService(String url, String username, String password, String apikey,
+			Class<E> clazz) {
+		JAXRSClientFactoryBean factory = new JAXRSClientFactoryBean();
+
+		factory.setAddress(url);
+
+		Map<String, String> headers = new HashMap<>();
+
+		if (StringUtils.isNotBlank(apikey)) {
+			headers.put("X-Redmine-API-Key", apikey);
+		} else if (StringUtils.isNotBlank(username)) {
+			factory.setUsername(username);
+			factory.setPassword(password);
+		}
+		
+		// FIXME error handling
+		
+		return factory.create(clazz);
+		
 	}
 }
